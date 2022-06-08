b63a34803f0c2f4cb0a3b03ab101ffda90b47e57
JungYT <52434353+JungYT@users.noreply.github.com>
2022-06-08 21:16:52 +0900

    220608

--- dynamics.py
+++ dynamics.py
@@ -2,6 +2,8 @@ import numpy as np
 import numpy.random as random
 
 from fym.core import BaseEnv, BaseSystem
+from fym.utils import rot
+from fym.agents import LQR
 
 class PointMass2D(BaseEnv):
     def __init__(self, cfg):
@@ -19,6 +21,82 @@ class PointMass2D(BaseEnv):
         self.speed.dot = -speed/self.tau_speed + cmd_speed/self.tau_speed
         self.yaw.dot = -yaw/self.tau_yaw + cmd_yaw/self.tau_yaw
 
+
+class PointMass2DLinear(BaseEnv):
+    def __init__(self, cfg):
+        super().__init__()
+        # self.pos = BaseSystem(cfg.init_pos)
+        # self.vel = BaseSystem(cfg.init_vel)
+        self.pos = BaseSystem(shape=(2,1))
+        self.vel = BaseSystem(shape=(2,1))
+        self.m = cfg.mass
+
+    def set_dot(self, force):
+        self.pos.dot = self.vel.state
+        self.vel.dot = force / self.m
+
+
+class Pendulum(BaseEnv):
+    def __init__(self):
+        super().__init__()
+        # self.th = BaseSystem(np.vstack([0.]))
+        # self.thdot = BaseSystem(np.vstack([0.]))
+        self.th = BaseSystem(np.array([0.]))
+        self.thdot = BaseSystem(np.array([0.]))
+        self.g = 10
+        self.m = 1.
+        self.l = 1.
+        self.max_u = 2.
+        self.max_speed = 8.
+
+    def set_dot(self, u):
+        th = self.th.state
+        thdot = self.thdot.state
+
+        thdot_tmp = -3 * self.g / (2 * self.l) * np.sin(th + np.pi) \
+            + 3. / (self.m * self.l ** 2) * u
+
+        self.th.dot = thdot
+        self.thdot.dot = np.clip(thdot_tmp, -self.max_speed, self.max_speed)
+
+
+class InvertedPendulum(BaseEnv):
+    def __init__(self):
+        super().__init__(dt=0.05, max_t=20)
+        self.pendulum = Pendulum()
+
+    def reset(self):
+        super().reset()
+        self.pendulum.th.state = randomize([[-np.pi, np.pi]])
+        self.pendulum.thdot.state = randomize([[-1, 1]])
+        obs = self.observe()
+        return obs
+
+    def step(self, u):
+        *_, done = self.update(u=u)
+        r = self.get_reward(u)
+        obs = self.observe()
+        return obs, r, done
+
+    def set_dot(self, t, u):
+        self.pendulum.set_dot(u)
+        reward = self.get_reward(u)
+        return dict(time=t, **self.observe_dict(), reward=reward, action=u)
+
+    def get_reward(self, u):
+        th = self.pendulum.th.state
+        thdot = self.pendulum.thdot.state
+        th = wrap(th)
+        r = -th ** 2 - 0.1 * thdot ** 2 - 0.001 * u ** 2
+        return r
+
+    def observe(self):
+        th = self.pendulum.th.state
+        thdot = self.pendulum.thdot.state
+        obs = np.hstack([ np.cos(th), np.sin(th), thdot ]).reshape(1, -1)
+        return obs
+
+
 class PointMass2DPathFollowing(BaseEnv):
     def __init__(self, cfg):
         super().__init__(dt=cfg.dt, max_t=cfg.max_t)
@@ -33,7 +111,7 @@ class PointMass2DPathFollowing(BaseEnv):
         self.distance_min = None
         self.obs = None
 
-    def reset(self ):
+    def reset(self):
         super().reset()
         self.quad.pos.state = np.vstack((
             random.uniform(
@@ -90,7 +168,7 @@ class PointMass2DPathFollowing(BaseEnv):
         y_T = distance.item() if tmp > 0 else -distance.item()
         # y_T = -np.sin(yaw_T)*(x - traj[0]) + np.cos(yaw_T)*(y - traj[1])
 
-        obs = np.hstack([y_T, e_yaw, curvature]).reshape(1,-1)
+        obs = np.vstack([y_T, e_yaw, curvature]).reshape(1,-1)
 
         self.obs = obs.squeeze()
         self.theta_min = theta
@@ -163,5 +241,165 @@ class PointMass2DPathFollowing(BaseEnv):
             tangent_set[i] = [tangent_x, tangent_y]
         return trajectory, theta_set, curvature_set, yaw_T_set, tangent_set
 
+
+class SimpleQuad(BaseEnv):
+    def __init__(self, cfg):
+        super().__init__()
+        self.pos = BaseSystem(cfg.init_pos)
+        self.vel = BaseSystem(cfg.init_vel)
+        self.euler = BaseSystem(cfg.init_euler)
+        self.thrust = BaseSystem(cfg.mass*9.81)
+        self.tau_euler = cfg.tau_euler
+        self.tau_thrust = cfg.tau_thrust
+        self.mass = cfg.mass
+        self.z = np.vstack((0., 0., 1.))
+        self.g = -9.81*self.z
+
+    def set_dot(self, cmd_f, cmd_euler, disturbance):
+        self.pos.dot = self.vel.state
+        phi, theta, psi = self.euler.state.squeeze()
+        R = rot.angle2dcm(psi, theta, phi)
+        force = self.thrust.state*R.dot(self.z)
+        self.vel.dot = (force + disturbance)/self.mass + self.g
+        self.euler.dot = (-self.euler.state + cmd_euler) / self.tau_euler
+        self.thrust.dot = (-self.thrust.state + cmd_f) / self.tau_thrust
+
+
+class SimpleQuadGuidance(BaseEnv):
+    def __init__(self, cfg):
+        super().__init__(dt=cfg.dt, max_t=cfg.max_t)
+        self.quad = SimpleQuad(cfg.quad)
+        self.cfg = cfg
+        self.reward = None
+        self.obs = None
+
+    def reset(self, goal):
+        super().reset()
+        self.quad.pos.state = randomize(self.cfg.quad.rand_pos)
+        self.quad.vel.state = randomize(self.cfg.quad.rand_vel)
+        self.quad.euler.state = randomize(self.cfg.quad.rand_euler)
+        obs = self.observe(goal)
+        return obs
+
+    def set_dot(self, t, action, cmd_psi, disturbance, goal):
+        cmd_f, cmd_euler = self.convert_action2cmd(action, cmd_psi)
+        self.quad.set_dot(cmd_f, cmd_euler, disturbance)
+
+        return dict(time=t, **self.observe_dict(), cmd_euler=cmd_euler, 
+                    cmd_f=cmd_f, action=action, disturbance=disturbance, 
+                    goal=goal)
+
+    def step(self, action, goal, cmd_psi):
+        disturbance = np.vstack((0., 0., 0.))
+        *_, done = self.update(
+            action=action,
+            cmd_psi=cmd_psi,
+            disturbance=disturbance,
+            goal=goal
+        )
+        obs = self.observe(goal)
+        reward = self.get_reward(obs)
+        return obs, reward, done
+
+    def convert_action2cmd(self, action, cmd_psi):
+        cmd_f, cmd_chi, cmd_gamma = action.squeeze()
+        z_des = rot.sph2cart2(1, cmd_chi, cmd_gamma)
+        cmd_phi, cmd_theta = self.convert_z2euler(z_des, cmd_psi)
+        cmd_euler = np.vstack((cmd_phi, cmd_theta, cmd_psi))
+        return cmd_f, cmd_euler
+
+    def convert_z2euler(self, z_des, psi):
+        z_n = rot.angle2dcm(psi, 0, 0).dot(z_des)
+        theta = np.arctan2(z_n[0], z_n[2]).item()
+        phi = np.arctan2(-z_n[1]*z_n[2], np.cos(theta)*(1-z_n[1]**2)).item()
+        return phi, theta
+
+    def observe(self, goal):
+        pos = self.quad.pos.state
+        vel = self.quad.vel.state
+        euler = self.quad.euler.state
+        e_pos = pos - goal
+        obs = np.vstack([e_pos, vel, euler]).reshape(1,-1)
+        return obs
+
+    def get_reward(self, obs):
+        e_pos = obs[0][0:3]
+        r = -np.sqrt(e_pos[0]**2 + e_pos[1]**2 + e_pos[2]**2)
+        return r
+
+    def logger_callback(self, t, action, cmd_psi, disturbance, goal):
+        obs = self.observe(goal)
+        reward = self.get_reward(obs)
+        phi, theta, psi = self.quad.euler.state.squeeze()
+        R = rot.angle2dcm(psi, theta, phi)
+        return dict(reward=reward, obs=obs.squeeze(), dcm=R)
+
+
+class PointMass2DLinearOptimal(BaseEnv):
+    def __init__(self, cfg):
+        super().__init__(dt=cfg.dt, max_t=cfg.max_t)
+        self.quad = PointMass2DLinear(cfg.quad)
+        self.cfg = cfg
+        self.B = cfg.quad.B
+        self.K, self.P = LQR.clqr(
+            cfg.quad.A,
+            cfg.quad.B,
+            np.zeros((4,4)),
+            np.eye(2)
+        )
+
+    def reset(self):
+        super().reset()
+        self.quad.pos.state = self.randomize(self.cfg.quad.rand_pos)
+        self.quad.vel.state = self.randomize(self.cfg.quad.rand_vel)
+        obs = self.observe()
+        return obs
+
+    def set_dot(self, t, action):
+        u = -self.B.T.dot(action)
+        self.quad.set_dot(u.reshape(-1, 1))
+
+        obs = self.observe()
+        LQR_lambda = self.P.dot(obs.squeeze())
+        LQR_u = -self.K.dot(obs.squeeze())
+        reward = self.get_reward(obs, action)
+        return dict(time=t, **self.observe_dict(), action=action, u=u,
+                    reward=reward, LQR_lambda=LQR_lambda, LQR_u=LQR_u,
+                    LQR_P=self.P)
+
+    def step(self, action):
+        *_, done = self.update(action=action)
+        obs = self.observe()
+        reward = self.get_reward(obs, action)
+        return obs, reward, done
+
+    def observe(self):
+        pos = self.quad.pos.state
+        vel = self.quad.vel.state
+        obs = np.vstack([pos, vel]).reshape(1,-1)
+        return obs
+
+    def get_reward(self, obs, action):
+        pos = obs[0][0:2]
+        vel = obs[0][2:4]
+        u = -self.B.T.dot(action)
+        r = -self.cfg.ddpg.reward_weight*pos.dot(vel) - u.dot(u)
+        return r
+
+    def randomize(self, bound):
+        length = len(bound)
+        vec = np.ones((length, 1))
+        for i in range(len(bound)):
+            vec[i] = random.uniform(low=bound[i][0], high=bound[i][1])
+        return vec
+
+
 def wrap(angle):
     return (angle+np.pi) % (2*np.pi) - np.pi
+
+def randomize(bound):
+    length = len(bound)
+    vec = np.ones((length, 1))
+    for i in range(len(bound)):
+        vec[i] = random.uniform(low=bound[i][0], high=bound[i][1])
+    return vec
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import numpy as np
 import random
-from types import SimpleNamespace as SN
 from pathlib import Path
 from tqdm import tqdm
-from datetime import datetime
-# from matplotlib import pyplot as plt
-import matplotlib
 from collections import deque
+import matplotlib
 
 import torch
 import torch.nn as nn
@@ -14,63 +11,13 @@ import torch.optim as optim
 import fym.logging as logging
 
 from dynamics import PointMass2DPathFollowing
+from dynamics import SimpleQuadGuidance
+from dynamics import PointMass2DLinearOptimal
+from dynamics import InvertedPendulum
 from postProcessing import PostProcessing
+import config
 
 matplotlib.use('Agg')
-
-def load_config():
-    cfg = SN()
-    cfg.dt = 0.1
-    cfg.max_t = 10.
-    cfg.dir = Path('log', datetime.today().strftime('%Y%m%d-%H%M%S'))
-    cfg.num_train = 20000
-    cfg.num_validate = 10
-    cfg.interval_validate = 1000
-
-    cfg.quad = SN()
-    cfg.quad.tau_speed = 0.1
-    cfg.quad.tau_yaw = 0.1
-    cfg.quad.init_pos = np.vstack((-8., 5.))
-    cfg.quad.init_speed = 0.5
-    cfg.quad.init_yaw = -np.pi/4
-    cfg.quad.rand_pos = [[-12., 12.], [-8., 8.]]
-    cfg.quad.rand_speed = [-15., 15.]
-    cfg.quad.rand_yaw = [-np.pi, np.pi]
-
-    cfg.traj = SN()
-    cfg.traj.A = 4.
-    cfg.traj.split_num  = 354
-
-    cfg.ddpg = SN()
-    cfg.ddpg.dim_state = 4
-    cfg.ddpg.dim_action = 2
-    cfg.ddpg.action_max = np.array([3., np.pi/2])
-    cfg.ddpg.action_min = np.array([-3., -np.pi/2])
-    cfg.ddpg.action_scaling = torch.from_numpy(cfg.ddpg.action_max).float()
-    cfg.ddpg.memory_size = 300000
-    cfg.ddpg.actor_lr = 0.0001
-    cfg.ddpg.critic_lr = 0.001
-    cfg.ddpg.batch_size = 64
-    cfg.ddpg.discount = 0.999
-    cfg.ddpg.softupdate_rate = 0.001
-    cfg.ddpg.terminate_condition = 10
-    cfg.ddpg.reward_weight = 2
-    cfg.ddpg.reward_max = 21
-    cfg.ddpg.reward_scaling = 10
-    cfg.ddpg.reward_bias = 11
-    # cfg.ddpg.actor_node = 64
-    # cfg.ddpg.critic_node = 64
-    cfg.ddpg.node_set = [64, 128, 256]
-
-    cfg.noise = SN()
-    cfg.noise.rho = 0.01
-    cfg.noise.mu = np.zeros(cfg.ddpg.dim_action)
-    cfg.noise.sigma = 1 / cfg.ddpg.action_max
-    cfg.noise.size = cfg.ddpg.dim_action
-    cfg.noise.dt = cfg.dt
-    cfg.noise.x0 = None
-    cfg.noise.tau = 200
-    return cfg
 
 
 class ActorNet(nn.Module):
@@ -89,7 +36,7 @@ class ActorNet(nn.Module):
         x1 = self.relu(self.bn1(self.lin1(state)))
         x2 = self.relu(self.bn2(self.lin2(x1)))
         x3 = self.tanh(self.lin3(x2))
-        x_scaled = x3 * self.cfg.action_scaling
+        x_scaled = (x3 + self.cfg.action_bias) * self.cfg.action_scaling
         return x_scaled
 
 class CriticNet(nn.Module):
@@ -239,8 +186,9 @@ def train(cfg, env, agent, noise, epi):
             cfg.ddpg.action_min
         )
         xn, r, done  = env.step(action)
-        r_scaled = (r + cfg.ddpg.reward_bias) / cfg.ddpg.reward_scaling
-        item = (x.squeeze(), action, r_scaled, xn.squeeze(), done)
+        # r_scaled = (r + cfg.ddpg.reward_bias) / cfg.ddpg.reward_scaling
+        # item = (x.squeeze(), action, r_scaled, xn.squeeze(), done)
+        item = (x.squeeze(), action, r, xn.squeeze(), done)
         agent.memorize(item)
         x = xn
         if len(agent.memory) > 5 * cfg.ddpg.batch_size:
@@ -256,7 +204,7 @@ def validate(cfg, env, agent, dir_save_env):
 
     x = env.reset()
     while True:
-        action = agent.get_action(x)
+        action = agent.get_action(x, net='target')
         xn, _, done = env.step(action)
         x = xn
         if done:
@@ -265,13 +213,19 @@ def validate(cfg, env, agent, dir_save_env):
 
 
 def main():
-    cfg = load_config()
-    env = PointMass2DPathFollowing(cfg)
-    cfg.traj.trajectory = env.trajectory
-    cfg.traj.theta_set = env.theta_set
-    cfg.traj.curvature_set = env.curvature_set
-    cfg.traj.yaw_T_set = env.yaw_T_set
-    cfg.traj.tangent_set = env.tangent_set
+    # cfg = config.path_follow()
+    # env = PointMass2DPathFollowing(cfg)
+    # cfg = config.simple_guidance()
+    cfg = config.pointmass_linear()
+    # cfg = config.pendulum()
+    # env = SimpleQuadGuidance(cfg)
+    env = PointMass2DLinearOptimal(cfg)
+    # env = InvertedPendulum()
+    # cfg.traj.trajectory = env.trajectory
+    # cfg.traj.theta_set = env.theta_set
+    # cfg.traj.curvature_set = env.curvature_set
+    # cfg.traj.yaw_T_set = env.yaw_T_set
+    # cfg.traj.tangent_set = env.tangent_set
     noise = OrnsteinUhlenbeckNoise(cfg.noise)
     post_processing = PostProcessing(cfg)
     node_set = cfg.ddpg.node_set
@@ -298,7 +252,10 @@ def main():
 
                     validate(cfg, env, agent, dir_save_env)
                     agent.save_params(dir_save_agent)
-                    post_processing.draw_plot(dir_save, dir_save_env)
+                    # post_processing.path_follow(dir_save, dir_save_env)
+                    # post_processing.simple_guidance(dir_save, dir_save_env)
+                    post_processing.pointmass_linear(dir_save, dir_save_env)
+                    # post_processing.pendulum(dir_save, dir_save_env)
                 post_processing.average_return()
         post_processing.compare_validate(Path(cfg.dir, f"{node}node"))
         print(f"end with {node} node")
